72ee21cec249f712ae2fd72c1a0a1e541c53148c
chandanasathish <chandanasathishnimblix@gmail.com>
2026-02-25 14:34:26 +0530

    Updated payment repository and removed unused files

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/repository/PaymentRepository.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/repository/PaymentRepository.java
@@ -3,12 +3,8 @@ package nimblix.in.HealthCareHub.repository;
 import java.util.Optional;
 import java.util.List;
 import nimblix.in.HealthCareHub.model.Payment;
-import nimblix.in.HealthCareHub.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
@@ -19,7 +15,7 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<Payment> findByAppointmentId(Long appointmentId);
 
     // Get all payments for a specific appointment (if needed later)
-    List<Payment> findAllByAppointment(Appointment appointment);
+    <Appointment> List<Payment> findAllByAppointment(Appointment appointment);
 
     // Get payments by status (SUCCESS, PARTIAL, REFUNDED)
     List<Payment> findByPaymentStatus(String paymentStatus);
